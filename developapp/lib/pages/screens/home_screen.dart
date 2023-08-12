ea19ed7065a22e53644d9c8f4ab51c762d878517
sude ilayda ozkara <sudeozkara@marun.edu.tr>
2023-08-12 23:48:08 +0300

    sliverappbar appbar düzeltildi

--- developapp/lib/pages/screens/home_screen.dart
+++ developapp/lib/pages/screens/home_screen.dart
@@ -5,42 +5,41 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Color(0xFFFDE456),
-        appBar: AppBar(
-          backgroundColor: Color.fromARGB(255, 228, 103, 176),
-          automaticallyImplyLeading: false,
-          leading: Image.asset('lib/images/logosarı.png'),
-          title: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 150),
-            child: Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(right: 150),
-                  child: Container(
-                      child: Text(
-                    "e v e l o p",
-                    style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.w900,
-                        color: Color(
-                          0xFFFDE456,
-                        )),
-                  )),
-                ),
-              ],
-            ),
-          ),
-        ),
         body: CustomScrollView(
           slivers: [
             SliverAppBar(
-                flexibleSpace: FlexibleSpaceBar(
-                  background: Container(
-                    color: Color(0xFF57209D),
-                  ),
+              backgroundColor: Color.fromARGB(255, 228, 103, 176),
+              automaticallyImplyLeading: false,
+              leading: Image.asset('lib/images/logosarı.png'),
+              title: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 150),
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 150),
+                      child: Container(
+                          child: Text(
+                        "e v e l o p",
+                        style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.w900,
+                            color: Color(
+                              0xFFFDE456,
+                            )),
+                      )),
+                    ),
+                  ],
                 ),
-                automaticallyImplyLeading: false,
-                expandedHeight: 300,
-                backgroundColor: Color.fromARGB(255, 228, 103, 176)),
+              ),
+              flexibleSpace: FlexibleSpaceBar(
+                background: Container(
+                  color: Color(0xFF57209D),
+                ),
+              ),
+
+              expandedHeight: 00,
+              // backgroundColor: Color.fromARGB(255, 228, 103, 176)
+            ),
             SliverToBoxAdapter(
                 child: Padding(
               padding: const EdgeInsets.all(8.0),
