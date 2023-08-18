78d627b4a87dc793410a2f4cc7a7eb7d723b41ab
sude ilayda ozkara <sudeozkara@marun.edu.tr>
2023-08-18 15:27:23 +0300

    değişiklikler eklendi

--- developapp/lib/pages/bar_items/nav_bar.dart
+++ developapp/lib/pages/bar_items/nav_bar.dart
@@ -1,15 +1,10 @@
-import 'dart:html';
-
-import 'package:developapp/pages/bar_items/profile_settings.dart';
-import 'package:file_picker/file_picker.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class NavBar extends StatelessWidget {
-  NavBar({super.key});
-  final user = FirebaseAuth.instance.currentUser!;
+  NavBar({Key? key}) : super(key: key);
+
+  final user = FirebaseAuth.instance.currentUser;
   void signUserOut() {
     FirebaseAuth.instance.signOut();
   }
@@ -23,36 +18,20 @@ class NavBar extends StatelessWidget {
         children: [
           UserAccountsDrawerHeader(
             accountName: Text("User122535"),
-            accountEmail: Text(user.email!),
-            currentAccountPicture: GestureDetector(
-              onTap: () async {
-                final results = await FilePicker.platform.pickFiles(
-                    allowMultiple: false,
-                    type: FileType.custom,
-                    allowedExtensions: ['png', 'jpg']);
-                if (results == null) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text("No File Selected")));
-                  return null;
-                }
-                final path = results.files.single.path;
-                final fileName = results.files.single.name;
-              },
-              child: CircleAvatar(
-                child: ClipOval(
-                    child: Image.network(
-                  'https://wallpaperaccess.com/full/6295120.jpg',
-                  width: 90,
-                  height: 90,
-                  fit: BoxFit.cover,
-                )),
+            accountEmail: Text(user!.email!),
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: NetworkImage(
+                'https://wallpaperaccess.com/full/6295120.jpg',
               ),
             ),
             decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: NetworkImage(
-                        'https://www.freecodecamp.org/news/content/images/2021/06/w-qjCHPZbeXCQ-unsplash.jpg'),
-                    fit: BoxFit.cover)),
+              image: DecorationImage(
+                image: NetworkImage(
+                  'https://www.freecodecamp.org/news/content/images/2021/06/w-qjCHPZbeXCQ-unsplash.jpg',
+                ),
+                fit: BoxFit.cover,
+              ),
+            ),
           ),
           ListTile(
             leading: Icon(Icons.favorite),
