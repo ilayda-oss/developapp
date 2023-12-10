9c320b77ce1dc55bb34b1f0caa8ddd2f60877593
sude ilayda ozkara <sudeozkara@marun.edu.tr>
2023-12-10 19:17:04 +0300

    ilan sayfası düzenlemeler

--- developapp/lib/pages/screens/post_screen.dart
+++ developapp/lib/pages/screens/post_screen.dart
@@ -2,14 +2,32 @@ import 'package:developapp/components/widgets/theme_modifier.dart';
 import 'package:developapp/provider/theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:image_picker/image_picker.dart';
 
 class PostScreen extends StatelessWidget {
+  final TextEditingController titleController = TextEditingController();
+  final TextEditingController categoryController = TextEditingController();
+
+  void openGallery() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      // Seçilen resmin yolu pickedFile.path olarak kullanılabilir.
+      // İstediğiniz işlemleri yapabilirsiniz.
+      print('Galeriden seçilen dosya yolu: ${pickedFile.path}');
+    } else {
+      print('Galeriden resim seçilmedi.');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final text = MediaQuery.of(context).platformBrightness == Brightness.dark
         ? 'DrakTheme'
         : 'LightTheme';
     final themeProvider = Provider.of<ThemeProvider>(context);
+
     MediaQuery.of(context).platformBrightness == Brightness.dark;
     return Scaffold(
       backgroundColor: themeProvider.bodyColor,
@@ -39,23 +57,27 @@ class PostScreen extends StatelessWidget {
               child: Row(
                 children: [
                   Container(
-                    height: 150,
+                    height: 120,
                     decoration: BoxDecoration(
-                      color: Color(0xFFF66DA7),
+                      color: themeProvider.textfieldColor,
                       borderRadius: BorderRadius.circular(15),
                     ),
                     child: Row(
                       children: [
-                        Container(
-                          height: 150,
-                          width: 150,
+                        InkWell(
+                          onTap: () {
+                            // Burada galeriyi açma işlemini gerçekleştirebilirsiniz.
+                            // Örneğin, galeriye gidilmesi için bir metot çağırabilirsiniz.
+                            openGallery();
+                          },
                           child: Container(
-                            padding: EdgeInsets.only(left: 30, right: 30),
-                            height: 105,
-                            child: Image.asset('lib/images/kamera.png'),
+                            width: 120,
+                            child: Container(
+                              padding: EdgeInsets.only(left: 30, right: 30),
+                              child: Image.asset('lib/images/kamera.png'),
+                            ),
                           ),
                         ),
-                        SizedBox(width: 10),
                       ],
                     ),
                   ),
@@ -66,53 +88,87 @@ class PostScreen extends StatelessWidget {
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
+                        SizedBox(
+                          height: 20,
+                        ),
                         Container(
-                          height: 30,
+                          height: 40,
                           child: TextField(
+                            controller: titleController,
+                            style: TextStyle(fontSize: 12),
+                            decoration: InputDecoration(
+                                labelText: 'Başlık',
+                                filled:
+                                    true, // TextField'ın iç rengini etkinleştirir
+                                fillColor: themeProvider
+                                    .textfieldColor, // TextField'ın iç rengini ayarlar
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(
+                                      25.0), // Dışındaki yumuşaklığı ayarlar
+                                ),
+                                suffixIcon: IconButton(
+                                  onPressed: () {
+                                    titleController.clear();
+                                  },
+                                  icon: const Icon(Icons.clear),
+                                )),
+                          ),
+                        ),
+                        SizedBox(height: 25),
+                        Container(
+                          height: 40,
+                          child: TextField(
+                            controller: categoryController,
                             onTap: () {
                               FocusScope.of(context).requestFocus(FocusNode());
                             },
                             style: TextStyle(fontSize: 12),
                             decoration: InputDecoration(
-                              labelText: 'Başlık',
-                              filled:
-                                  true, // TextField'ın iç rengini etkinleştirir
-                              fillColor: Color.fromARGB(255, 255, 141,
-                                  190), // TextField'ın iç rengini ayarlar
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(
-                                    15.0), // Dışındaki yumuşaklığı ayarlar
-                              ),
-                            ),
+                                labelText: 'Kategori/Alan',
+                                filled:
+                                    true, // TextField'ın iç rengini etkinleştirir
+                                fillColor: themeProvider
+                                    .textfieldColor, // TextField'ın iç rengini ayarlar
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(
+                                      25.0), // Dışındaki yumuşaklığı ayarlar
+                                ),
+                                suffixIcon: IconButton(
+                                  onPressed: () {
+                                    categoryController.clear();
+                                  },
+                                  icon: const Icon(Icons.clear),
+                                )),
                           ),
                         ),
-                        SizedBox(height: 50),
-                        Container(
-                          height: 30,
-                          child: TextField(
-                            onTap: () {
-                              FocusScope.of(context).requestFocus(FocusNode());
-                            },
-                            style: TextStyle(fontSize: 12),
-                            decoration: InputDecoration(
-                              labelText: 'Kategori/Alan',
-                              filled:
-                                  true, // TextField'ın iç rengini etkinleştirir
-                              fillColor: Color.fromARGB(255, 255, 141,
-                                  190), // TextField'ın iç rengini ayarlar
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(
-                                    15.0), // Dışındaki yumuşaklığı ayarlar
-                              ),
-                            ),
-                          ),
-                        ),
+                        SizedBox(height: 35),
+
                         // Buraya başka widget'lar ekleyebilirsiniz.
                       ],
                     ),
                   ),
                 ],
               ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                Padding(
+                  padding: themeProvider.switchImagesMargin,
+                  child: Container(
+                    width: 300,
+                    height: 250, // Sabit kutunun yüksekliği
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(themeProvider
+                            .switchImages), // Resim dosyasının yolu
+                        fit: BoxFit.cover, // Resmi kaplamak için uygun fit türü
+                      ),
+                    ), // Kutu rengi
+                  ),
+                ),
+              ],
             ),
           ),
         ],
--- developapp/lib/provider/theme_provider.dart
+++ developapp/lib/provider/theme_provider.dart
@@ -9,8 +9,8 @@ class ThemeProvider extends ChangeNotifier {
   }
 
   Color get textfieldColor => isDarkMode
-      ? Color.fromARGB(255, 255, 159, 199)
-      : Color.fromARGB(255, 183, 129, 255);
+      ? Color.fromARGB(255, 183, 129, 255)
+      : Color.fromARGB(255, 248, 186, 186);
   Color get primaryColor => Color(0xFFF66DA7);
   Color get appBarColor => isDarkMode ? Color(0xFFF66DA7) : Color(0xFFF66DA7);
   Color get bodyColor => isDarkMode ? Color(0xFF57209D) : Color(0xFFFDE456);
@@ -23,6 +23,14 @@ class ThemeProvider extends ChangeNotifier {
   Color get textColor => isDarkMode ? Color(0xFFFDE456) : Color(0xFFFDE456);
   Color get buttonColor =>
       isDarkMode ? const Color(0xFF57209D) : Color(0xFFFDE456);
+  String get switchImages =>
+      isDarkMode ? 'lib/images/kiz.png' : 'lib/images/erkek.png';
+
+  EdgeInsets get switchImagesMargin {
+    return isDarkMode
+        ? EdgeInsets.only(top: 177, right: 254)
+        : EdgeInsets.only(top: 177, left: 287);
+  }
 
   // Color.fromARGB(255, 228, 103, 176);
 }
